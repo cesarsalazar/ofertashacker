81be3eba17b64e08728728c7ca190051d960e8bb
Rogelio Guzman <chinog9@gmail.com>
2011-05-13 16:32:08 -0500

    Added twitter

---
+++ config/initializers/bitly.rb
@@ -0,0 +1,3 @@
+Bitly.use_api_version_3
+
+$bitly = Bitly.new("ofertashacker", "R_b81c9578176bcc672027ded11e2acda8")
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,8 @@
+require "twitter"
+
+Twitter.configure do |config|
+  config.consumer_key = "lZvfYCyac6NkJlfQZqzg"
+  config.consumer_secret = "tXxNfxmpqECmSmDbPz7FUkiDrerhBtGkUDjHzYvnaU"
+  config.oauth_token = "285148989-rMbFsdUm9XzJNiYqK20AqifiVRbdApQwySFdnxBd"
+  config.oauth_token_secret = "EBFzaxwgKsgskgHbZWxDQ4KaXgbybfbclPhhYCZyQ"
+end
